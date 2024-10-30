5a9b6eead609f2eab0f26c72f54d4f6ccab80cd8
Chicken-is-my-Jam-8 <s1347556@monmouth.edu>
2024-10-30 14:03:21 -0400

    Update valid_password.py

--- valid_password.py
+++ valid_password.py
@@ -9,21 +9,20 @@ has_digit = False
 
 user_password = input("please enter your password: ")
 
-if user_password.isalpha() == True:
-    pass
-else: has_digit = True
+if len(user_password) >= 7:
+    correct_length = True
 
 for x in user_password:
     if x.isupper() == True:
         has_uppercase = True
-    else:
-        pass
     if x.islower() == True:
         has_lowercase = True
-    else:
-        pass
+    if x.isdigit() == True:
+        has_digit = True
 
-if has_uppercase == False or has_lowercase == False or has_digit == False:
+if has_uppercase == False or has_lowercase == False :
+    print("False")
+elif has_digit == False or correct_length == False:
     print("False")
 else:
     print("True")
